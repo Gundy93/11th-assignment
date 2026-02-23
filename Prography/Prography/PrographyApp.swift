a361a966d64109b74d954d5cea0c10c22ada0ab8
Gundy <korea1880@naver.com>
2026-02-23 21:12:39 +0900

    [PRO-1] FEAT: 프로젝트 파일 생성

---
+++ Prography/Prography/PrographyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrographyApp.swift
+//  Prography
+//
+//  Created by Jun Young Lee on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct PrographyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

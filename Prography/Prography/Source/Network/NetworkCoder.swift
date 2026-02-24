d2f911fa29c4e5a48f1f74745bc44c41fec69a4c
Gundy <korea1880@naver.com>
2026-02-24 23:04:11 +0900

    [PRO-2] FEAT: 요청간에 인코딩과 디코딩을 담당한 NetworkCoder 구현

---
+++ Prography/Prography/Source/Network/NetworkCoder.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkCoder.swift
+//  Prography
+//
+//  Created by Jun Young Lee on 2/24/26.
+//
+
+import Foundation
+
+enum NetworkCoder {
+    static let encoder = JSONEncoder()
+    static let decoder = JSONDecoder()
+}

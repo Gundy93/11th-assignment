27abae968b060ec65727da7726aa639d8abe7357
Gundy <korea1880@naver.com>
2026-02-26 19:32:21 +0900

    [PRO-6] FEAT: 일정 목록 조회 API 요청 응답 및 관련 타입 구현

---
+++ Prography/Prography/Source/Model/Session.swift
@@ -0,0 +1,16 @@
+//
+//  Session.swift
+//  Prography
+//
+//  Created by Jun Young Lee on 2/26/26.
+//
+
+import Foundation
+
+struct Session {
+    let id: Int
+    let title: String
+    let date: Date
+    let location: String
+    let status: SessionStatus
+}

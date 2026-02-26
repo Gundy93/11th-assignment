51ae7138bd9e1444412066179b645bd98a62ff13
Gundy <korea1880@naver.com>
2026-02-26 10:49:12 +0900

    [PRO-4] FEAT: 로그인 API 요청 본문 타입 SignInRequest 구현

---
+++ Prography/Prography/Source/APIService/Auth/Request/SignInRequest.swift
@@ -0,0 +1,11 @@
+//
+//  SignInRequest.swift
+//  Prography
+//
+//  Created by Jun Young Lee on 2/26/26.
+//
+
+struct SignInRequest: Encodable {
+    let loginId: String
+    let password: String
+}

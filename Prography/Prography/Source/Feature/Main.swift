814c650f28e042106938033a14928da9bb8cd507
Gundy <korea1880@naver.com>
2026-02-26 17:56:58 +0900

    [PRO-5] FEAT: 메인 화면 UI 구현

--- Prography/Prography/Source/Feature/Main.swift
+++ Prography/Prography/Source/Feature/Main.swift
@@ -5,7 +5,13 @@
 //  Created by Jun Young Lee on 2/26/26.
 //
 
+import SwiftUI
 import ComposableArchitecture
+
+enum Tab: CaseIterable {
+    case schedule
+    case attendance
+}
 
 @Reducer
 struct MainFeature {
@@ -17,11 +23,7 @@ struct MainFeature {
     enum Action: BindableAction {
         case binding(BindingAction<State>)
         case tabSelected(Tab)
-    }
-    
-    enum Tab {
-        case schedule
-        case attendance
+        case scanQRCodeButtonTapped
     }
     
     var body: some Reducer<State, Action> {
@@ -35,7 +37,166 @@ struct MainFeature {
             case .tabSelected(let tab):
                 state.selectedTab = tab
                 return .none
+                
+            case .scanQRCodeButtonTapped:
+                return .none
             }
         }
     }
 }
+
+struct MainView: View {
+    @Bindable var store: StoreOf<MainFeature>
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            tabView
+            tabBar
+        }
+        .background(AppColor.white.color)
+    }
+    
+    private var tabView: some View {
+        ZStack {
+            schedule
+                .opacity(store.selectedTab == .schedule ? 1 : 0)
+            attendance
+                .opacity(store.selectedTab == .attendance ? 1 : 0)
+        }
+        .padding(
+            .bottom,
+            52
+        )
+        .frame(maxHeight: .infinity)
+    }
+    
+    private var schedule: some View {
+        Text("schedule")
+    }
+    
+    private var attendance: some View {
+        Text("attendance")
+    }
+    
+    private var tabBar: some View {
+        ZStack(alignment: .top) {
+            HStack(spacing: 0) {
+                ForEach(Tab.allCases, id: \.self) { tab in
+                    tabItem(tab)
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 52)
+            .background {
+                AppColor.white.color
+                    .shadow(
+                        color: AppColor.black.color.opacity(0.04),
+                        radius: 2,
+                        x: 0,
+                        y: -2
+                    )
+            }
+            .padding(
+                .top,
+                26
+            )
+            
+            floatingButton
+        }
+    }
+    
+    private func tabItem(_ tab: Tab) -> some View {
+        VStack(spacing: 4) {
+            tabItemIcon(tab)
+                .image(
+                    size: 20,
+                    appColor: tabItemIconColor(tab)
+                )
+            tabItemLabel(tab)
+        }
+        .frame(maxWidth: .infinity)
+        .padding(
+            .top,
+            4
+        )
+        .onTapGesture {
+            store.send(.tabSelected(tab))
+        }
+    }
+
+    private func tabItemLabel(_ tab: Tab) -> some View {
+        Text(Texts.tabItem(tab))
+            .appFont(
+                AppFont.p4Regular,
+                appColor: tabItemIconColor(tab)
+            )
+            .minimumScaleFactor(0.5)
+    }
+    
+    private var floatingButton: some View {
+        Button {
+            store.send(.scanQRCodeButtonTapped)
+        } label: {
+            Circle()
+                .fill(AppColor.primary.color)
+                .frame(
+                    width: 52,
+                    height: 52
+                )
+                .shadow(
+                    color: AppColor.black.color.opacity(0.1),
+                    radius: 2,
+                    x: 0,
+                    y: 0
+                )
+                .overlay {
+                    AppIcon.qrCode.image(
+                        size: 24,
+                        appColor: .white
+                    )
+                }
+        }
+    }
+}
+
+// MARK: UI Properties
+extension MainView {
+    private func tabItemIcon(_ tab: Tab) -> AppIcon {
+        switch tab {
+        case .schedule:
+                .schedule
+        case .attendance:
+                .attendance
+        }
+    }
+    
+    private func tabItemIconColor(_ tab: Tab) -> AppColor {
+        tab == store.selectedTab ? .primary : .gray50
+    }
+}
+
+// MARK: Types
+extension MainView {
+    enum Texts {
+        static func tabItem(_ tab: Tab) -> String {
+            switch tab {
+            case .schedule:
+                "세션 일정"
+            case .attendance:
+                "출결 현황"
+            }
+        }
+    }
+}
+
+#if DEBUG
+#Preview {
+    MainView(
+        store: Store(
+            initialState: MainFeature.State()
+        ) {
+            MainFeature()
+        }
+    )
+}
+#endif
